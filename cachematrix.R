869ebbfb18ce4ce8a91f7deca7478392719f79de
Haley <52020477+haleygm@users.noreply.github.com>
2020-03-20 19:10:13 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,11 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is assignment 2 for coursera week 3 of R Programming.
+## Below are paired functions. The first function creates a series of functions. 
+## These include changing the matrix data/inverse and returning the matrix data/inverse. 
+## The second function relies on the values of the prior function to either get the cached inverse or calculate a new inverse.
 
 ## Function that creates the functions to be used later in the cacheSolve function 
-## including getting/setting the inverse of the matrix
+## including returning/changing the matrix data/matrix inverse
+
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL #must create default value 
@@ -19,7 +22,7 @@ makeCacheMatrix <- function(x = matrix()) {
        getinverse = getinverse)
 }
 
-
+## Function that returns the following: 
 ## If no cached matrix or new matrix set, calculates inverse and returns the value
 ## If cached matrix exists, returns message and the cached inverse 
 
